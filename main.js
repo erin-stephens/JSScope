82e66ca1f931eaf5ab93e76a2b60eab4d2f8f99b
Erin Stephens <erebstephens@gmail.com>
2022-10-12 19:06:56 -0500

    second exercise

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log("This is fun!");
+const conjunction = function (firstWord, secondWord) {
+    const conjoinedWord = `${firstWord} ${secondWord}`
+    console.log(conjoinedWord)
+}
+
+conjunction("Master", "Card")
