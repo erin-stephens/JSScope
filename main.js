61df99f09e8e82058407731d39d793d9618b8b19
Erin Stephens <erebstephens@gmail.com>
2022-10-12 19:56:37 -0500

    stopped here

--- main.js
+++ main.js
@@ -24,3 +24,19 @@ conjunction("Master", "Card")
 
 
 console.log(HTMLRepresentation)
+
+const llamaNamer = function () {
+    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+    const randomizer = Math.floor(Math.random() * 7)
+
+    const namer = function () {
+        const suffix = " the Llama"
+        const name = possibleNames[randomizer]
+        return name + suffix
+    }
+
+    
+}
+
+let nameMaker = llamaNamer()
+console.log(nameMaker())
