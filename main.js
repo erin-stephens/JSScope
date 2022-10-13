1f3078b0ea66d8617fd4b4bf33c73432c1e903b2
Erin Stephens <erebstephens@gmail.com>
2022-10-12 19:58:38 -0500

    edits

--- main.js
+++ main.js
@@ -25,6 +25,25 @@ conjunction("Master", "Card")
 
 console.log(HTMLRepresentation)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const llamaNamer = function () {
     const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
     const randomizer = Math.floor(Math.random() * 7)
